ce3be0ba734a1bad250ae4e1d8edc343a3adcf97
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-12 02:44:17 -0600

    Create function to get player choice
    Create getHumanChoice function
    Add prompt to input choice
    Add method to prompt to force lower case
    Add while statement to keep asking for correct input

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@ function getComputerChoice() {
     let computerChoice = '';
     let computerSeed = Math.floor((Math.random() * 3) + 1);
     if (computerSeed === 1)  {
-        return computerChoice = "Rock";
+        return computerChoice = "rock";
     } else if (computerSeed === 2 ) {
-        return computerChoice = "Paper";
+        return computerChoice = "paper";
     } else {
-        return computerChoice = "Scrissors";
+        return computerChoice = "scrissors";
     }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Select rock, paper, or scissors!").toLowerCase();
+    
+    while (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
+        humanChoice = prompt("Please type the word 'rock', 'paper', or 'scissors'.").toLowerCase();
+    }
+
+    return humanChoice;
 }
