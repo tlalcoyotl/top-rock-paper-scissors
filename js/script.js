a397de0c66532e326dbbdf6b718c0dc4949b1c51
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-12 02:03:27 -0600

    Create getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let computerChoice = '';
+    let computerSeed = Math.floor((Math.random() * 3) + 1);
+    if (computerSeed === 1)  {
+        return computerChoice = "Rock";
+    } else if (computerSeed === 2 ) {
+        return computerChoice = "Paper";
+    } else {
+        return computerChoice = "Scrissors";
+    }
+}
