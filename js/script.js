23362231facf3ac2e42828c3d3f15dc6e64b9fcf
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-12 04:52:52 -0600

    Create function to run a game of 5 rounds
    Add function to get each choice when called
    Add function to play the game again
    Complete project

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let computerScore = 0;
 function getComputerChoice() {
     let computerChoice = '';
     let computerSeed = Math.floor((Math.random() * 3) + 1);
+    //Get a different choice according to the random integer x 3
     if (computerSeed === 1)  {
         return computerChoice = "rock";
     } else if (computerSeed === 2 ) {
@@ -20,12 +21,14 @@ function getHumanChoice() {
     let humanChoice = prompt("Select rock, paper, or scissors!").toLowerCase();
     
     while (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
+        // Prompt again if input was not rock, paper, or scissors
         humanChoice = prompt("Please type the word 'rock', 'paper', or 'scissors'!").toLowerCase();
     }
 
     return humanChoice;
 }
 
+// Play a single round
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === "rock" && computerChoice === "scissors" ||
         playerChoice === "paper" && computerChoice === "rock" ||
@@ -33,24 +36,57 @@ function playRound(playerChoice, computerChoice) {
             // If player wins increase humanScore
             console.log(`You win this round! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
             humanScore++;
-            getScore();
         } else if (playerChoice === computerChoice) {
             // If there's a tie don't increase anything
             console.log("That's a tie!")
-            getScore();
         } else {
             // If player loses increase computerScore
             console.log(`You lose this round! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} loses against ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
             computerScore++;
-            getScore();
-        }
+            }
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+let humanSelection;
+let computerSelection;
 
-playRound(humanSelection, computerSelection);
+// Function to change humanSelection and computerSelection each round
+function getChoices () {
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+}
 
+// Function to show score
 function getScore() {
     console.log("Player: " + humanScore + '  Computer: ' + computerScore);
+}
+
+// Function to create a console.log that will show who is the winner
+function getWinner() {
+    if (humanScore === computerScore) {
+        console.log("This game is a tie!");
+    } else if (humanScore < computerScore) {
+        console.log("You lost the game!");
+    } else {
+        console.log("You won the game!");
+    }
+}
+
+// Play a game of 5 rounds
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        getChoices();
+        playRound(humanSelection, computerSelection);
+        getScore();
+    }
+    getWinner();
+    console.log("Play again by entering playAgain() in the console!")
+}
+
+playGame();
+
+// Function to play again
+function playAgain() {
+    humanScore = 0;
+    computerScore = 0;
+    playGame();
 }
