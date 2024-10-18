0949b0764f9b23e631373288e19bf7ce6c46e364
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-18 03:43:33 -0600

    Set up to play round by pressing a button

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
 // Set variables for score
 let humanScore = 0;
 let computerScore = 0;
+let playerSelection = document.getElementById('player')
+let buttons = document.querySelectorAll('button');
+playerSelection.addEventListener('click', playRound);
 
-// Create function for getting the computer choice
 function getComputerChoice() {
     let computerChoice = '';
     let computerSeed = Math.floor((Math.random() * 3) + 1);
-    //Get a different choice according to the random integer x 3
     if (computerSeed === 1)  {
         return computerChoice = "rock";
     } else if (computerSeed === 2 ) {
         return computerChoice = "paper";
     } else {
         return computerChoice = "scissors";
-    }
+    };
 }
 
-// Create function for getting player input
-function getHumanChoice() {
-    let humanChoice = prompt("Select rock, paper, or scissors!").toLowerCase();
-    
-    while (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
-        // Prompt again if input was not rock, paper, or scissors
-        humanChoice = prompt("Please type the word 'rock', 'paper', or 'scissors'!").toLowerCase();
+function playRound(e) {
+    let playerChoice;
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    if (e.target.classList.contains('btn')) {
+        playerChoice = e.target.parentNode.id;
     }
-
-    return humanChoice;
+    roundResult(playerChoice, computerChoice);
 }
-
 // Play a single round
-function playRound(playerChoice, computerChoice) {
+function roundResult(playerChoice, computerChoice) {
     if (playerChoice === "rock" && computerChoice === "scissors" ||
         playerChoice === "paper" && computerChoice === "rock" ||
         playerChoice === "scissors" && computerChoice === "paper") {
@@ -43,16 +41,7 @@ function playRound(playerChoice, computerChoice) {
             // If player loses increase computerScore
             console.log(`You lose this round! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} loses against ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
             computerScore++;
-            }
-}
-
-let humanSelection;
-let computerSelection;
-
-// Function to change humanSelection and computerSelection each round
-function getChoices () {
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
+            };
 }
 
 // Function to show score
@@ -71,22 +60,3 @@ function getWinner() {
     }
 }
 
-// // Play a game of 5 rounds
-// function playGame() {
-//     for (let i = 0; i < 5; i++) {
-//         getChoices();
-//         playRound(humanSelection, computerSelection);
-//         getScore();
-//     }
-//     getWinner();
-//     console.log("Play again by entering playAgain() in the console!")
-// }
-
-// playGame();
-
-// // Function to play again
-// function playAgain() {
-//     humanScore = 0;
-//     computerScore = 0;
-//     playGame();
-// }
